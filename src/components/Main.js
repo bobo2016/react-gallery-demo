253ca9193286cde35ba57d8690b6ff8b9adcf202
Charles <jb.zhang1234@gmail.com>
2016-10-17 23:44:57 -0400

    add images and json data

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,35 @@ require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+//Get image related data
+let imageDatas = require('../data/imageDatas.json');
+
+/*let yeomanImage = require('../images/yeoman.png');*/
+
+
+// Get image Url from imageDatas.json filename
+//Use self-invoke function for function only execute once
+imageDatas = (function genImageURL(imageDatasArr) {
+	for(var i = 0, j = imageDatasArr.length; i < j; i++){
+		var singleImageData = imageDatasArr[i];
+
+		singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+
+		imageDatasArr[i] = singleImageData;
+	}
+
+	return imageDatasArr;
+})(imageDatas)
+
+
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+    	<section className="stage">
+    		<section className="img-sec"></section>
+    		<nav className="controller-nav"></nav>
+    	</section>
     );
   }
 }
