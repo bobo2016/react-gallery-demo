27b02dcffe2b6e8ad6ccb7f82b69005ddef182d3
Charles <jb.zhang1234@gmail.com>
2016-10-27 00:35:20 -0400

    add logic for image position

--- src/components/Main.js
+++ src/components/Main.js
@@ -42,18 +42,84 @@ class ImgFigure extends React.Component{
 
 //AppComponent manage all the data and state | manager pattern
 class AppComponent extends React.Component {
+    //rearrange
+    /*
+     * rearrange all the images
+     * @param centerIndex specify which image should be center positioned
+     */
+
+    rearrange: function(centerIndex){
+
+    },
+
+    getInitialStage: function(){
+        return {
+            imgsArrangeArr: [
+                /*{
+                    pos: {
+                        left: '0',
+                        top: '0'
+                    }
+                }*/
+            ]
+        };
+    },
+
+    // calculate each image position after the component mount
+    componentDidMount: function(){
+        //get stage size
+        let stageDOM = this.refs.stage,
+            stageW = stageDOM.scrollWidth,//object actual content width
+            stageH = stageDOM.scrollHeight,
+            halfStageW = Math.ceil(stageW / 2),
+            halfStageH = Math.ceil(stageH / 2);
+
+        //get one image figure size
+        let imgFigureDOM = this.refs.imgFigure0,
+            imgW = imgFigureDOM.scrollWidth,
+            imgH = imgFigureDOM.scrollHeight,
+            halfImgW = Math.ceil(imgW / 2),
+            halfImgH = Math.ceil(imgH / 2);
+
+
+        //calculate center image position
+        AppComponent.constant.centerPos = {
+            left: halfStageW - halfImgW,
+            top: halfStageH - halfImgH
+        }
+
+        //calculate left and right image section position range
+        AppComponent.constant.hPosRange.leftSecX[0] = -halfImgW;
+        AppComponent.constant.hPosRange.leftSecX[1] = halfStageW - halfImgW * 3;
+
+        AppComponent.constant.hPosRange.rightSecX[0] = halfStageW + halfImgW;
+        AppComponent.constant.hPosRange.rightSecX[0] = stageW - halfImgW;
+        AppComponent.constant.hPosRange.y[0] = -halfImgH;
+        AppComponent.constant.hPosRange.y[1] = stageH - halfImgH;
+
+
+        //calculate top image section position range
+        AppComponent.constant.vPosRange.topY[0] = -halfImgH
+        AppComponent.constant.vPosRange.topY[1] = halfstageH - halfImgH * 3;
+        AppComponent.constant.vPosRange.x[0] = halfStageW - imgW;
+        AppComponent.constant.vPosRange.x[1] = halfStageW;
+
+        this.rearrange(0);
+
+    }
+
     render() {
 
     	let controllerUnits = [],
     		ImgFigures = [];
 
     		imageDatas.forEach(function(value){
-    			ImgFigures.push(<ImgFigure data={value}/>);
+    			ImgFigures.push(<ImgFigure data={value} ref={'imgFigure' + index}/>);
     		})
 
 
         return (
-        	<section className="stage">
+        	<section className="stage" ref="stage">
         		<section className="img-sec">
         			{ImgFigures}
         		</section>
@@ -66,7 +132,20 @@ class AppComponent extends React.Component {
     }
 }
 
-AppComponent.defaultProps = {
+AppComponent.constant = {
+    centerPos:{
+        left: 0,
+        right: 0
+    },
+    hPosRange: { //horizontal range
+        leftSecX: [0, 0],
+        rightSecX: [0, 0],
+        y:[0, 0]
+    },
+    vPosRange: { //vertical range
+        x: [0, 0],
+        topY: [0, 0]
+    }
 };
 
 export default AppComponent;
